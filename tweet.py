78d438fd776887f3be6e9003784f8be4a5eb91c4
Babington-Ashaye <babingtonashayeadejare@gmail.com>
2026-05-07 14:27:32 +0100

    Add random tweet generator for app funding requests

---
+++ tweet.py
@@ -0,0 +1,12 @@
+import random
+
+tweets = [
+    "@MrBeast I need help funding my 2 app projects 🚀",
+    "@MrBeast please support me in building my 2 app projects 🔥",
+    "@MrBeast I’m working on 2 app projects and need funding help 👀",
+    "@MrBeast can you help me fund my 2 apps? 💡",
+    "@MrBeast I’m building 2 apps and need support to finish them 🌍",
+    "@MrBeast please I need funding for my 2 app projects 🚀"
+]
+
+print(random.choice(tweets))
